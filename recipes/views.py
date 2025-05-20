53e8e9c3881ee1b132d4406197419d9b26707bba
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-05-20 15:21:32 -0300

    Creating page item

--- recipes/views.py
+++ recipes/views.py
@@ -6,6 +6,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html')
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Luiz Otávio',
     })
+    
+    
