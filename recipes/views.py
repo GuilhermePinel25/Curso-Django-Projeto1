455886108c6c1f72bf5d26dfc334a8f166332bd7
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-05-20 14:31:27 -0300

    URL Testing

--- recipes/views.py
+++ recipes/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'recipes/pages/home.html')
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Luiz Otávio',
+    })
