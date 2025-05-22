212f97bef1eea0174d164dc57408df6f2eefd115
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-05-22 12:14:07 -0300

    modifying category page titles

--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,23 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from utils.recipes.factory import make_recipe
-from .models import Recipe
- 
+
+from .models import Recipe, Category
+
+
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
     })
 
 def category(request,category_id):
-    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    category = get_object_or_404(Category, id=category_id)
+    recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'category': category,
     })
 
 def recipe(request, id):
