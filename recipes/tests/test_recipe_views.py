33506529b0ea61c6c79986c402aecfbbbdee6a7d
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-05-23 11:59:38 -0300

    Testing status code and template in views

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -4,6 +4,7 @@ from django.urls import resolve, reverse
 from recipes import views
 
 class RecipeViewsTest(TestCase):
+    
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
@@ -15,5 +16,17 @@ class RecipeViewsTest(TestCase):
     def test_recipe_recipe_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+        
+    def test_recipe_home_view_resturn_status_code_200_OK(self):
+        resolve = self.client.get(reverse('recipes:home'))
+        self.assertEqual(resolve.status_code, 200)  
+        
+    def test_recipe_home_view_loads_correct_template(self):
+        resolve = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(resolve, 'recipes/pages/home.html')
+    
+    # def test_recipe_home_view_resturn_status_code_200_OK(self):
+    #     view = resolve(reverse('recipes:home'))
+    #     self.assertIs(view.func, views.home)
         
         
