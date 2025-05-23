dfa4d6cf6a4df7b1e506ec2b6daa4c9803d4cfd6
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-05-23 16:39:54 -0300

    Creating data for testing

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -3,6 +3,8 @@ from django.urls import resolve, reverse
 
 from recipes import views
 
+from recipes.models import Category, Recipe, User
+
 class RecipeViewsTest(TestCase):
     
     # ========= Tests from home ==========
@@ -22,6 +24,39 @@ class RecipeViewsTest(TestCase):
         resolve = self.client.get(reverse('recipes:home'))
         self.assertIn('No recipes found here!', resolve.content.decode('utf-8'))
         
+    def test_recipe_home_template_loads_recipes(self): #criando dados ficticios para teste
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name= 'user',
+            last_name= 'name',
+            username= "username",
+            password= '123456',
+            email= 'user@email.com'
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,          
+            title = 'Recipe Title',
+            description = 'Recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutos',
+            servings = 5,
+            servings_unit = 'Porções',
+            preparation_steps = 'Recipe Preparation Steps',
+            preparation_steps_is_html = False,
+            is_published = True,
+        )
+        
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+        
+        self.assertIn('Recipe Title', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)
+    
     # ========= Tests from category ==========
         
     def test_recipe_category_view_function_is_correct(self):
