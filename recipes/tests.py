b52b1acdbaedaa0557c09c8e6f4d5f7077e3abf3
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-05-23 10:24:04 -0300

    creating urls test

--- recipes/tests.py
+++ recipes/tests.py
@@ -3,5 +3,14 @@ from django.urls import reverse
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+    
+    def test_recipe_category_url_is_correct(self):
+        url = reverse('recipes:category', kwargs={'category_id': 2})
+        self.assertEqual(url, '/recipes/category/2/')
+    
+    def test_recipe_detail_url_is_correct(self):
+        for i in range(0,5):
+            url = reverse('recipes:recipe', kwargs={'id': i})
+            self.assertEqual(url, '/recipes/'f'{i}/')
