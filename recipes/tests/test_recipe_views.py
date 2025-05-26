b4d2ee2b36552534d353d30e38bc3fc7b2daf4b7
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-05-26 18:31:47 -0300

    final test views

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -63,6 +63,21 @@ class RecipeViewsTest(RecipeTestBase):
         content = response.content.decode('utf-8')
         
         self.assertIn(needed_title, content)
+        
+    def test_recipe_category_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+        
+        response = self.client.get(
+            reverse(
+                'recipes:category', 
+                kwargs={'category_id':recipe.category.id}
+                )
+            )
+        
+        #Check if one recipe exists
+        self.assertEqual(response.status_code, 404)
     
     # ========= Tests from recipe/detail ==========
         
@@ -88,6 +103,21 @@ class RecipeViewsTest(RecipeTestBase):
         content = response.content.decode('utf-8')
         
         self.assertIn(needed_title, content)
+        
+    def test_recipe_detail_template_dont_load_recipe_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        recipe = self.make_recipe(is_published=False)
+        
+        response = self.client.get(
+            reverse(
+                'recipes:recipe', 
+                kwargs={'id':recipe.id}
+                )
+            )
+        
+        #Check if one recipe exists
+        self.assertEqual(response.status_code, 404)
     
         
         
