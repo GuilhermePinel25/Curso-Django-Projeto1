eae03cbbfffe1afe23509fcc46ad13b61bc4907c
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-06-10 14:57:14 -0300

    ListView

--- recipes/views.py
+++ recipes/views.py
@@ -1,21 +1,47 @@
-from django.shortcuts import render,get_list_or_404, get_object_or_404
-from utils.recipes.factory import make_recipe
-from .models import Recipe, Category
-from django.http import Http404
-from django.db.models import Q
-from django.core.paginator import Paginator
-from utils.recipes.pagination import make_pagination
-from django.contrib import messages
-
 import os
+from typing import List
 
-PER_PAGES = os.environ.get('PER_PAGE')
+from django.db.models import Q
+from django.http.response import Http404
+from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.views.generic import ListView
+from utils.recipes.pagination import make_pagination
+
+from recipes.models import Recipe
+
+PER_PAGE = int(os.environ.get('PER_PAGE', 6))
+
+
+class RecipeListViewBase(ListView):
+    model = Recipe
+    context_object_name = 'recipes'
+    ordering = ['-id']
+    template_name = 'recipes/pages/home.html'
+
+    def get_queryset(self, *args, **kwargs):
+        qs = super().get_queryset(*args, **kwargs)
+        qs = qs.filter(
+            is_published=True,
+        )
+        return qs
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        page_obj, pagination_range = make_pagination(
+            self.request,
+            ctx.get('recipes'),
+            PER_PAGE
+        )
+        ctx.update(
+            {'recipes': page_obj, 'pagination_range': pagination_range}
+        )
+        return ctx
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     
     
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/home.html', context={
         'recipes': page_obj,
@@ -30,7 +56,7 @@ def category(request,category_id):
             ).order_by('-id')
         )
     
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
     
     return render(request, 'recipes/pages/category.html', context={
         'recipes': page_obj,
@@ -61,7 +87,7 @@ def search(request):
         is_published=True
     ).order_by('-id')
     
-    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGES)
+    page_obj, pagination_range = make_pagination(request, recipes, PER_PAGE)
 
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
