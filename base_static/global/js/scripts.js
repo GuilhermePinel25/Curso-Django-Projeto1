b738ac5e7dc5bca8bc6deb27453f6b585d243290
GuilhermePinel25 <guilhermespinel@gmail.com>
2025-06-13 17:26:26 -0300

    menu lateral finalizado com logout form

--- base_static/global/js/scripts.js
+++ base_static/global/js/scripts.js
@@ -43,6 +43,20 @@ function my_scope() {
 
   })();
 
+  (() => {
+    const authorsLogoutLinks = document.querySelectorAll('.authors-logout-link');
+    const formLogout = document.querySelector('.form-logout');
+
+    for (const link of authorsLogoutLinks) {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        formLogout.submit();
+        
+      })
+    }
+  })();
+
 }
 
 my_scope();
